71604b061d080d33f9803cd68a0eba3eb55f2140
wogud <wogud9601@gmail.com>
2023-09-07 22:30:28 +0900

    third commit

---
+++ 2weak.py
@@ -0,0 +1,9 @@
+  # Example: Python's simplicity
+print("Hello, World!")
+
+age =18
+   # Example: Code readability
+if age < 18:
+       print("You are a minor.")
+else:
+       print("You are an adult.")
